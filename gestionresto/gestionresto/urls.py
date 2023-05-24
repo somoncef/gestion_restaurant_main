52ae15e01c1b31cfc7be312946a79bf5da013413
moncef <72748046+somoncef@users.noreply.github.com>
2023-05-24 01:01:54 +0000

    11

--- gestionresto/gestionresto/urls.py
+++ gestionresto/gestionresto/urls.py
@@ -1,3 +1,5 @@
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path,include
 from . import views
@@ -12,4 +14,4 @@ urlpatterns = [
     path('', views.home),
     path('signup/', views.signup),
    
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- gestionresto/gestionresto/views.py
+++ gestionresto/gestionresto/views.py
@@ -1,13 +1,48 @@
-from django.shortcuts import render
-import requests
+
+from django.contrib.auth import authenticate,login
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib import messages
+from items import views as v
 
 
 
-def index(respons):
-    return render(respons,'login.html')
 
-def signup(respons):
-    return render(respons,'signup.html')
+def index(req):
+    if req.method == 'POST':
+        username=req.POST['username']
+        passw=req.POST['pass']
+        user=authenticate(username=username, password=passw)
+        if user is not None:
+            login(req, user)
+        else:
+            messages.error(req,"bad username or password")
+
+        return redirect(v.default)
+        
+    return render(req,'login.html',)
+    
+
+def signup(req):
+    if req.method == 'POST':
+        use=req.POST.get('username')
+        lname=req.POST.get('lname')
+        fname=req.POST.get('fname')
+        email=req.POST.get('email')
+        pass1=req.POST.get('pass1')
+        pass2=req.POST.get('pass2')
+        
+        if pass1!=pass2:
+            messages.error(req ,"passwords do not match")
+           
+        myuser=User.objects.create_user(use,email,pass1)
+        myuser.first_name=fname
+        myuser.last_name=lname
+        myuser.save() 
+        messages.success(req , "Successfully created")
+        return redirect('login.html')
+            
+    return render(req,'signup.html')
 
 def home(respons):
     return render(respons,'base.html')
