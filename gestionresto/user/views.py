d4b50b106a2ce8eca8a3db59d7ec15fb0cad029b
moncef <72748046+somoncef@users.noreply.github.com>
2023-06-19 19:22:06 +0000

    last3

--- gestionresto/user/views.py
+++ gestionresto/user/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import User,UserProfile
 from reservasion.models import Reservation
-
+from .forms import ImageForm
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -31,4 +31,36 @@ def profileupdate(request):
     reservation = Reservation.objects.get(user=request.user)
     return render(request, 'profile-update.html', {'profile': profile, "reservation": reservation})
 
+def index(request):
+    if request.method == "POST":
+        form=ImageForm(data=request.POST,files=request.FILES)
+    if form.is_valid():
+        form.save()
+        obj=form.instance
+        return render(request,"profile.html",{"obj":obj})
+    else:
+        form=ImageForm()
+    Userp=UserProfile.objects.all()
+    return render(request,"profile.html",{"Userp":Userp,"form":form})
 
+@login_required
+def update(req):    
+    if req.method == 'POST':
+        lname = req.POST.get('last_name')
+        fname = req.POST.get('first_name')
+        email = req.POST.get('email')
+        phone = req.POST.get('phone')
+        address = req.POST.get('address')
+    
+    
+        myuser = UserProfile.objects.get(user=req.user)
+        myuser.user.first_name = fname
+        myuser.user.last_name = lname
+        myuser.user.email = email
+        myuser.phone = phone
+        myuser.address = address
+        myuser.save()
+
+        return redirect('profile.html')
+     
+    return render(req, 'profile.html')
